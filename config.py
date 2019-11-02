c621844acef4549dca547b81c6f7ca5570020751
Dan Halverson <daniel.m.halverson@gmail.com>
2019-11-02 09:12:25 -0500

    adding in new files

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -9,3 +8,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['daniel.m.halverson@gmail.com']
---
+++ readme.md
@@ -0,0 +1,2 @@
+#### Microblog  
+Based on the tutuorial https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world, I'm learning some python.
